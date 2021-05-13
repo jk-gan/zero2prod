2019eb5d259404f1d7bde8b1f0246ac622a9e276
jk-gan <junkai@hey.com>
2021-05-13 13:35:55 +0800

    test: add test case for health_check endpoint
    Signed-off-by: jk-gan <junkai@hey.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use std::net::TcpListener;
+use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind the 8000 port");
+
+    run(listener)?.await
 }
